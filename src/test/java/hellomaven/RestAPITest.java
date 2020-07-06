c18f0e956a226caaaa541d51737aa5bff1883e4b
ksk <ksk.ee25@gmail.com>
2020-07-06 08:49:28 +0530

    First commit

---
+++ src/test/java/hellomaven/RestAPITest.java
@@ -0,0 +1,20 @@
+package hellomaven;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+
+	@Test
+	public void postjira()
+	{
+		System.out.println("postJIRA");
+		
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+	
+}
---
+++ src/test/java/hellomaven/seleniumTest.java
@@ -0,0 +1,20 @@
+package hellomaven;
+
+import org.testng.annotations.Test;
+
+public class seleniumTest {
+
+	@Test
+	public void browserAutomation()
+	{
+		System.out.println("browserAutomationtest");
+		
+	}
+	
+	@Test
+	public void elementsUI()
+	{
+		System.out.println("elementsUI");
+	}
+	
+}
