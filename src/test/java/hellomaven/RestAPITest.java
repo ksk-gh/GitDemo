804a1625706fb082ae61f54a543087f336992af1
ksk <ksk.ee25@gmail.com>
2020-07-08 08:18:14 +0530

    This is commited by guy from asian time zone

--- src/test/java/hellomaven/RestAPITest.java
+++ src/test/java/hellomaven/RestAPITest.java
@@ -7,8 +7,9 @@ public class RestAPITest {
 	@Test
 	public void postjira()
 	{
-		System.out.println("postJIRA");
-		
+		System.out.println("postJIRA1");
+		System.out.println("postJIRA2");
+		System.out.println("postJIRA3");
 	}
 	
 	@Test
