62d65131eda69c112321dca7fa59e9968c7027f3
ksk <ksk.ee25@gmail.com>
2020-07-09 22:08:46 +0530

    Pushed from Git stuff

--- src/test/java/hellomaven/RestAPITest.java
+++ src/test/java/hellomaven/RestAPITest.java
@@ -10,6 +10,12 @@ public class RestAPITest {
 		System.out.println("postJIRA1");
 		System.out.println("postJIRA2");
 		System.out.println("postJIRA3");
+		// pushed from git stuff
+		System.out.println("postJIRA4");
+		System.out.println("postJIRA5");
+		System.out.println("postJIRA6");
+		
+		
 	}
 	
 	@Test
